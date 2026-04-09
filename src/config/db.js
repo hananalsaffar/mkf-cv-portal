169c20baa8ef74eb34bcedb0d92bad0074560232
Hanan Alsaffar <hanan@Hanans-MacBook-Pro.local>
2026-04-09 14:42:38 +0300

    added DB port support

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-// MySQL connection setup
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -7,7 +6,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT 
 });
 
 module.exports = db;
